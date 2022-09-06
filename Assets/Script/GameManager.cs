39c6b060eb8e244f8418da7f2812435e1e316320
Artyu24 <arthurpejoiane@hotmail.fr>
2022-09-06 17:08:09 +0200

    Add GameManager Script

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Slider slider;  
+    List<int> notePos = new List<int>();
+
+    void OnDrawGizmos()
+    {
+        if(slider != null)
+        {
+
+        }
+    }
+}
+
