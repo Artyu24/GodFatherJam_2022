d7ed9b6f8c106a58ccd6499a8c99d9f15d77b01c
AeroPorc <hugopv.smp@gmail.com>
2022-09-07 19:40:37 +0200

    Add sliderspwanning
    V1
    
    Slider spawn pas encore en bonne position + handle pas encore link

--- Assets/Scripts/Sliderspawner.cs
+++ Assets/Scripts/Sliderspawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,13 +15,24 @@ public class Sliderspawner : MonoBehaviour
     [SerializeField]
     public GameObject BottomLeftSlider;
 
-    public float Fpart;
+    public bool existingTopRight = false;
+    public bool existingBottomRight = false;
+    public bool existingTopLeft = false ;
+    public bool existingBottomLeft = false;
+
+    private GameObject topleftslider;
+    private GameObject bottomleftslider;
+    private GameObject toprightslider;
+    private GameObject bottomrightslider;
+
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
@@ -28,7 +40,68 @@ public class Sliderspawner : MonoBehaviour
     {
         foreach (SliderNote note in GetComponent<GameManager>().PartOne)
         {
+            if (note.valueNote <= 300)
+            {
+                switch (note.sliderChoose)
+                {
+                    case 1:
+                        if (existingTopLeft == false)
+                        {
+                            topleftslider = Instantiate(TopLeftSlider);
+                            existingTopLeft = true;
+                            if (note.valueNote < 10)
+                            {
+                                GameObject.Destroy(topleftslider);
+                                Debug.LogWarning("Destroyed");
+                                existingTopLeft = false;
+                            }
 
+
+                        }
+                        break;
+                    case 2:
+                        if (existingBottomLeft == false)
+                        {
+                            bottomleftslider = Instantiate(BottomLeftSlider);
+                            existingBottomLeft = true;
+                            if(note.valueNote < 10)
+                            {
+                                GameObject.Destroy(bottomleftslider);
+                                Debug.LogWarning("Destroyed");
+                                existingBottomLeft = false;
+                            }
+                        }
+                        break;
+                    case 3:
+                        if(existingTopRight == false)
+                        {
+                            toprightslider = Instantiate(TopRightSlider);
+                            existingTopRight = true;
+                            if (note.valueNote < 10)
+                            {
+                                GameObject.Destroy(toprightslider);
+                                Debug.LogWarning("Destroyed");
+                                existingTopRight = false;
+                            }
+                        }
+                        break;
+                    case 4:
+                        if(existingBottomRight == false)
+                        {
+                            bottomrightslider = Instantiate(BottomRightSlider);
+                            existingBottomRight=true;
+                            if(note.valueNote < 10)
+                            {
+                                GameObject.Destroy(bottomrightslider);
+                                Debug.LogWarning("Destroyed");
+                                existingBottomRight = false;
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
