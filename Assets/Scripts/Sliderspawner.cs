803f8ad1d5e85990f3d70161ed888f90f87e7ffa
AeroPorc <hugopv.smp@gmail.com>
2022-09-07 15:47:14 +0200

    Add new scene and slider spawner

---
+++ Assets/Scripts/Sliderspawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sliderspawner : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject TopRightSlider;
+    [SerializeField]
+    public GameObject BottomRightSlider;
+    [SerializeField]
+    public GameObject TopLeftSlider;
+    [SerializeField]
+    public GameObject BottomLeftSlider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
